6434bba72454b7b03679e0126ffff18c50045548
JimmyWxzzz <xuzhuwang354@gmail.com>
2022-10-07 17:40:34 -0400

    Deployed

--- src/main/java/com/sweepy/RedisCache/RedisConfig.java
+++ src/main/java/com/sweepy/RedisCache/RedisConfig.java
@@ -1,18 +1,32 @@
 package com.sweepy.RedisCache;
 
 import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.connection.RedisStandaloneConfiguration;
 import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
 import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.repository.configuration.EnableRedisRepositories;
 import org.springframework.data.redis.serializer.GenericJackson2JsonRedisSerializer;
 import org.springframework.data.redis.serializer.StringRedisSerializer;
 
 import java.io.Serializable;
 
 @Configuration
+@EnableConfigurationProperties({RedisDBProperties.class})
+@EnableRedisRepositories
 public class RedisConfig {
+
+    @Bean()
+    public LettuceConnectionFactory redisConnectionFactory(RedisDBProperties redisDBProperties) {
+        RedisStandaloneConfiguration redisStandaloneConfiguration = new RedisStandaloneConfiguration();
+        redisStandaloneConfiguration.setHostName(redisDBProperties.getHost());
+        redisStandaloneConfiguration.setPort(redisDBProperties.getPort());
+        redisStandaloneConfiguration.setPassword(redisDBProperties.getPassword());
+        return new LettuceConnectionFactory(redisStandaloneConfiguration);
+    }
     @Bean
     public RedisTemplate<String, Object> redisTemplate(RedisConnectionFactory redisConnectionFactory){
 
---
+++ src/main/java/com/sweepy/RedisCache/RedisDBProperties.java
@@ -0,0 +1,13 @@
+package com.sweepy.RedisCache;
+
+import lombok.Data;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+
+@ConfigurationProperties(prefix = "redis")
+@Data
+public class RedisDBProperties {
+    private String host;
+    private Integer port;
+    private String password;
+}
+
